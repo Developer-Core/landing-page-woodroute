58a97d77e1e6574b787832afaac1fed450218f92
psulca <psulcasanchez@gmail.com>
2026-04-24 16:09:25 -0500

    feat: nav-bar with mobile menu and language switcher

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 import { initI18n } from './i18n-loader.js'
+import { initNavBar } from './nav-bar.js'
 
 const lang = detectLanguage()
 document.documentElement.lang = lang
 
-initI18n(lang)
+await initI18n(lang)
+initNavBar()
 
 function detectLanguage() {
   const stored = localStorage.getItem('wr-lang')

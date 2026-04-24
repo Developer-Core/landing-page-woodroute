1a6ccd8cfbedd55bbeda3318d7e91eb92a6ab5e6
psulca <psulcasanchez@gmail.com>
2026-04-24 16:19:24 -0500

    feat: pricing section with monthly/annual billing toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 import { initI18n } from './i18n-loader.js'
 import { initNavBar } from './nav-bar.js'
+import { initPricingSection } from './pricing-section.js'
 
 const lang = detectLanguage()
 document.documentElement.lang = lang
 
 await initI18n(lang)
 initNavBar()
+initPricingSection()
 
 function detectLanguage() {
   const stored = localStorage.getItem('wr-lang')
